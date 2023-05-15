ef9254211c6a4e3d50ae661dacbd770ed2c5e3e4
Logan <loganbrinsmead1115@gmail.com>
2023-05-14 20:16:43 -0700

    animated current state and current number of steps

--- index.js
+++ index.js
@@ -1,250 +1,283 @@
-// a class for the Machine Tape
-class Tape {
-    constructor(string) {
-        console.log(typeof (string))
-        this.tape = Tape.parse(string);
-    }
-    // get the tape
-    get status() {
-        return this.tape.join("");
-    }
+$(document).ready(function () {
 
-    // setter for tape
-    // set tape(string) {
-    //     console.log(typeof(string))
+    // a class for the Machine Tape
+    class Tape {
+        constructor(string) {
+            console.log(typeof (string))
+            this.tape = Tape.parse(string);
+        }
+        // get the tape
+        get status() {
+            return this.tape.join("");
+        }
 
-    //     this.tape = Tape.parse(string.join()); 
-    // }
+        // setter for tape
+        // set tape(string) {
+        //     console.log(typeof(string))
 
-    //lets us push a blank symbol to the back of the array
-    extendRight() {
+        //     this.tape = Tape.parse(string.join()); 
+        // }
 
-        this.tape.push("_");
-    }
-    //lets us push a blank symbol to the front of the array
-    extendLeft() {
-        this.tape.unshift("_");
-    }
+        //lets us push a blank symbol to the back of the array
+        extendRight() {
 
-    // write a cell of the tape
-    updateCell(cellIdx, newCell) {
-        this.tape[cellIdx] = newCell;
-    }
-    //takes the string splits it by character, making it a lot easier to work with
-    static parse(string) {
-        console.log(typeof (string), string)
-        return string.split("");
-    }
+            this.tape.push("_");
+        }
+        //lets us push a blank symbol to the front of the array
+        extendLeft() {
+            this.tape.unshift("_");
+        }
 
-}
+        // write a cell of the tape
+        updateCell(cellIdx, newCell) {
+            this.tape[cellIdx] = newCell;
+        }
+        //takes the string splits it by character, making it a lot easier to work with
+        static parse(string) {
+            console.log(typeof (string), string)
+            return string.split("");
+        }
 
-// contains the logic that models after a turing machine head
-class Head {
-    // state will be stored in the zeroeth index of the input to the head
-    // idx/location will be stored in the 1st index
-    // input argument is a string
-    // example: string === "s1 0"
-    constructor(input) {
-        let splitInput = Head.splitInput(input);
-        let state = splitInput[0];//state
-        let idx = parseInt(splitInput[1]);//currIdx
-        this.updateHeadInfo(state, idx);
     }
 
-    get currentHead() {
-        return `Current state at in head: ${this.state}, current Index in head: ${this.idx}`;
+    // contains the logic that models after a turing machine head
+    class Head {
+        // state will be stored in the zeroeth index of the input to the head
+        // idx/location will be stored in the 1st index
+        // input argument is a string
+        // example: string === "s1 0"
+        constructor(input) {
+            let splitInput = Head.splitInput(input);
+            let state = splitInput[0];//state
+            let idx = parseInt(splitInput[1]);//currIdx
+            this.updateHeadInfo(state, idx);
+        }
+
+        get currentHead() {
+            return `Current state at in head: ${this.state}, current Index in head: ${this.idx}`;
+        }
+
+        get currState() {
+            return this.state;
+        }
+
+        get currIdx() {
+            return this.idx;
+        }
+
+        updateHeadInfo(state, idx) {
+            this.state = state;
+            this.idx = idx;
+        }
+
+        // parses the input and splits it for every space
+        static splitInput(input) {
+            console.log(input, typeof (input))
+            return input.split(" ");
+        }
     }
 
-    updateHeadInfo(state, idx) {
-        this.state = state;
-        this.idx = idx;
-    }
+    class Machine {
 
-    // parses the input and splits it for every space
-    static splitInput(input) {
-        console.log(input, typeof (input))
-        return input.split(" ");
-    }
-}
+        constructor(tape, head, rules) {
+            this.tape = tape;
+            this.head = head;
+            this.rules = rules;
+            this._runStatus = true;
+            this._numSteps = 0;
+        }
 
-class Machine {
+        get status() {
+            return this.tape.tape + " " + this.head.currentHead;
+        }
+        get runStatus() {
+            return this._runStatus
+        }
+        set runStatus(runStatus) {
+            this._runStatus = runStatus;
+        }
 
-    constructor(tape, head, rules) {
-        this.tape = tape;
-        this.head = head;
-        this.rules = rules;
-        this._runStatus = true;
-    }
+        set numSteps(numSteps) {
+            this._numSteps = numSteps;
+        }
 
-    get status() {
-        return this.tape.tape + " " + this.head.currentHead;
-    }
-    get runStatus() {
-        return this._runStatus
-    }
-    set runStatus(runStatus) {
-        this._runStatus = runStatus;
-    }
+        get currState() {
+            return this.head.currState;
+        }
+
+        get currIdx() {
+            return this.head.currIdx;
+        }
+
+        get numSteps() {
+            return this._numSteps;
+        }
 
 
-    moveHead(direction) {
-        if (this.head.idx == 0 && (direction == "L" || direction == "l")) {
-            this.tape.extendLeft();
-            this.head.idx -= 1;
+        moveHead(direction) {
+            if (this.head.idx == 0 && (direction == "L" || direction == "l")) {
+                this.tape.extendLeft();
+                this.head.idx -= 1;
+
+            }
+            else if (this.head.idx == this.tape.tape.length - 1 && (direction == "R" || direction == "r")) {
+                this.tape.extendRight();
+                this.head.idx += 1;
+
+            } else if (direction == "L" || direction == "l") {
+                this.head.idx -= 1;
+            } else if (direction == "R" || direction == "r") {
+                this.head.idx += 1;
+            }
+
+
 
         }
-        else if (this.head.idx == this.tape.tape.length - 1 && (direction == "R" || direction == "r")) {
-            this.tape.extendRight();
-            this.head.idx += 1;
 
-        } else if (direction == "L" || direction == "l") {
-            this.head.idx -= 1;
-        } else if (direction == "R" || direction == "r") {
-            this.head.idx += 1;
-        }
-        
+        /**
+         * Returns true if we have a rule for the current state
+         * and input
+         * else it returns false
+         */
 
+        stepRules() {
+            let ruleExist = false;
+            //console.log(`thsi is head state ${this.head.state}`);
+            // if ((this.rules[this.head.state] && this.rules[this.head.state][this.tape.tape[this.head.idx]]) ||("*" in this.rules)) {
 
-    }
-
-    /**
-     * Returns true if we have a rule for the current state
-     * and input
-     * else it returns false
-     */
-
-    stepRules() {
-        let ruleExist = false;
-        //console.log(`thsi is head state ${this.head.state}`);
-        // if ((this.rules[this.head.state] && this.rules[this.head.state][this.tape.tape[this.head.idx]]) ||("*" in this.rules)) {
-
-        //     return true;
-        // }//else{
-        //     console.log("testing 123...")
-            for(const e in this.rules[this.head.state]){
+            //     return true;
+            // }//else{
+            //     console.log("testing 123...")
+            for (const e in this.rules[this.head.state]) {
                 console.log(e);
-                if((this.rules[this.head.state] && this.rules[this.head.state][this.tape.tape[this.head.idx]]) || (e === "*")){
+                if ((this.rules[this.head.state] && this.rules[this.head.state][this.tape.tape[this.head.idx]]) || (e === "*")) {
                     ruleExist = true;
                 }
             }
-        // }
+            // }
 
-        return ruleExist;
-    }
+            return ruleExist;
+        }
 
 
-    step() {
+        step() {
 
-        let newState;
-        let writeSymbol;
-        let direction;
-        //console.log(this.rules[this.head.state]);
-        
-        //this.rules[this.head.state][this.tape.tape[this.head.idx]][0] !== "*")
-        if (this.rules[this.head.state][this.tape.tape[this.head.idx]] != null) {
-            if(this.rules[this.head.state][this.tape.tape[this.head.idx]][0] === "*"){
-                newState = this.head.state;
-            }else{
-            newState = this.rules[this.head.state][this.tape.tape[this.head.idx]][0];
-            }
-            if(this.rules[this.head.state][this.tape.tape[this.head.idx]][1] === "*"){
-                writeSymbol = this.tape.tape[this.head.idx];
+            // increment the number of steps
+            this.numSteps = this.numSteps+=1;
 
-            }else {
-            writeSymbol = this.rules[this.head.state][this.tape.tape[this.head.idx]][1];
-            }
-            direction = this.rules[this.head.state][this.tape.tape[this.head.idx]][2];
-           
-        } else {
-            //this is in case the only state we have to make a move on is *
-            for(const e in this.rules[this.head.state]){
-                if(e === "*" && !(this.rules[this.head.state][this.tape.tape[this.head.idx]] in this.rules[this.head.state])){
+            let newState;
+            let writeSymbol;
+            let direction;
+            //console.log(this.rules[this.head.state]);
 
-                    if(this.rules[this.head.state][e][0] === "*"){
-                        newState = this.head.state;
-                    }else{
-                    newState = this.rules[this.head.state][e][0];
+            //this.rules[this.head.state][this.tape.tape[this.head.idx]][0] !== "*")
+            if (this.rules[this.head.state][this.tape.tape[this.head.idx]] != null) {
+                if (this.rules[this.head.state][this.tape.tape[this.head.idx]][0] === "*") {
+                    newState = this.head.state;
+                } else {
+                    newState = this.rules[this.head.state][this.tape.tape[this.head.idx]][0];
+                }
+                if (this.rules[this.head.state][this.tape.tape[this.head.idx]][1] === "*") {
+                    writeSymbol = this.tape.tape[this.head.idx];
+
+                } else {
+                    writeSymbol = this.rules[this.head.state][this.tape.tape[this.head.idx]][1];
+                }
+                direction = this.rules[this.head.state][this.tape.tape[this.head.idx]][2];
+
+            } else {
+                //this is in case the only state we have to make a move on is *
+                for (const e in this.rules[this.head.state]) {
+                    if (e === "*" && !(this.rules[this.head.state][this.tape.tape[this.head.idx]] in this.rules[this.head.state])) {
+
+                        if (this.rules[this.head.state][e][0] === "*") {
+                            newState = this.head.state;
+                        } else {
+                            newState = this.rules[this.head.state][e][0];
+                        }
+                        if (this.rules[this.head.state][e][1] === "*") {
+
+                            writeSymbol = this.tape.tape[this.head.idx];
+                            //console.log(`thsi is the write symbol ${writeSymbol}`);
+
+                        } else {
+                            writeSymbol = this.rules[this.head.state][e][1];
+                            //console.log(`thsi is write symbol ${writeSymbol}`);
+                        }
+
+                        direction = this.rules[this.head.state][e][2];
                     }
-                    if(this.rules[this.head.state][e][1] === "*"){
-                        
-                        writeSymbol = this.tape.tape[this.head.idx];
-                        //console.log(`thsi is the write symbol ${writeSymbol}`);
-        
-                    }else {
-                        writeSymbol = this.rules[this.head.state][e][1];
-                        //console.log(`thsi is write symbol ${writeSymbol}`);
-                    }
-                    
-                    direction = this.rules[this.head.state][e][2];
                 }
             }
+
+            // console.log(`thsi is write symbol ${writeSymbol}`);
+            this.tape.updateCell(this.head.idx, writeSymbol);
+            this.head.state = newState;
+            this.moveHead(direction);
+
+            // while (this.head.idx < 0) {
+            //     this.moveHead("r");
+            // }
+
+
         }
-
-       // console.log(`thsi is write symbol ${writeSymbol}`);
-        this.tape.updateCell(this.head.idx, writeSymbol);
-        this.head.state = newState;
-        this.moveHead(direction);
-        
-        while(this.head.idx < 0) {
-            this.moveHead("r");
+        /**
+         * This function should allow the user to take a single step 
+         * in the turing machine
+         */
+        oneStep() {
+            if (this.stepRules()) {
+                this.step();
+            }
         }
+        /**
+         * have a boolean variable runTrue
+         * pass it to setter
+         * check to see if it is true
+         * and if it is false 
+         * 
+         */
 
+        run() {
+            while (this.stepRules()) {
+                console.log(this.status);
+                this.step();
 
-    }
-    /**
-     * This function should allow the user to take a single step 
-     * in the turing machine
-     */
-    oneStep() {
-        if (this.stepRules()) {
-            this.step();
-        }
-    }
-    /**
-     * have a boolean variable runTrue
-     * pass it to setter
-     * check to see if it is true
-     * and if it is false 
-     * 
-     */
+                // changing the current state on frontend
+                changeCurState(this.head.state);
+                changeCurSteps(this.numSteps);
 
-    run() {
-        while (this.stepRules()) {
+            }
+            console.log("Final tape condition and final state");
             console.log(this.status);
-            this.step();
-            
+            console.log(this.head.state);
+            console.log(this.tape.tape[this.head.idx])
+
+
+
+            //console.log(this.stepRules());
+            //console.log(this.rules[this.head.state]);
+            // console.log(this.rules[this.head.state][this.tape.tape[this.head.idx]]);
+            // console.log(this.tape.tape[this.head.idx]);
         }
-        console.log("Final tape condition and final state");
-        console.log(this.status);
-        console.log(this.head.state);
-        console.log(this.tape.tape[this.head.idx])
-        
-        
-        
-        //console.log(this.stepRules());
-        //console.log(this.rules[this.head.state]);
-       // console.log(this.rules[this.head.state][this.tape.tape[this.head.idx]]);
-      // console.log(this.tape.tape[this.head.idx]);
+
+        // run machine at half speed
+        halfSpeedRun() {
+            setInterval(run, 1000);
+        }
+
     }
 
-    // run machine at half speed
-    halfSpeedRun() {
-        setInterval(run, 1000);
-    }
-
-}
-
-/**
- * format of JSON that contains rules:
- * "state-in-head": {
- *      "symbol-head-reads": ["state-head-changes-to", "symbol-head-writes", "direction-head-moves"]
- *      "symbol-head-reads": ["state-head-changes-to", "symbol-head-writes", "direction-head-moves"]     
- *      "symbol-head-reads": ["state-head-changes-to", "symbol-head-writes", "direction-head-moves"]     
- * }
- */
+    /**
+     * format of JSON that contains rules:
+     * "state-in-head": {
+     *      "symbol-head-reads": ["state-head-changes-to", "symbol-head-writes", "direction-head-moves"]
+     *      "symbol-head-reads": ["state-head-changes-to", "symbol-head-writes", "direction-head-moves"]     
+     *      "symbol-head-reads": ["state-head-changes-to", "symbol-head-writes", "direction-head-moves"]     
+     * }
+     */
 
 
-$(document).ready(function () {
     let rules = {};
     // parsing the information in the textarea and inserting that into a JSON object
     function parseProgram() {
@@ -309,7 +342,7 @@ $(document).ready(function () {
     }
     function isObject(item) {
         return (item && typeof item === 'object' && !Array.isArray(item));
-      }
+    }
 
     // This is the function that binds to the reset button
     // TODO: I am adding the functionality for display without any testing
@@ -321,10 +354,43 @@ $(document).ready(function () {
     //$("#StringInput").val()
     let head = new Head("0 0");
 
+    let activeTapeArea = $(`<div id="ActiveTapeArea"></div>`);
+    let activeTape = $(`<pre id="ActiveTapeArea" class="tape"></pre>`);
+    let machineHead = $(`<div id="MachineHead"></div>`);
+    let headTop = $(`<div class="HeadTop"></div>`);
+    let headBody = $(`<div class="HeadBody">Head</div>`);
+
+    $(machineHead).append($(headTop));
+    $(machineHead).append($(headBody));
+
+    activeTapeArea.append($(activeTape));
+    activeTapeArea.append($(machineHead));
+
+    let leftTape = $(`<pre id="leftTape" class="tape"></pre>`);
+    let rightTape = $(`<pre id="RightTape" class="tape">0010001</pre>`);
+
+    let tapeValues = $("#TapeValues");
+
+    // function to change active state in the tape on the frontend
+    function changeActiveArea(indexOfHeadPosition) {
+
+    }
+
+    // function to change the number of steps on the frontend
+    function changeCurSteps(numSteps) {
+        console.log("numSteps: ", numSteps, " numSteps as string: ", numSteps.toString())
+        $("#MachineCurrentStep").text(numSteps.toString());
+    }
+
+    // function to change the current state on the frontend
+    function changeCurState(curState) {
+        $("#MachineCurrentState").text(curState);
+    }
+
     $("#RunButton").on("click", function (e) {
         rules = {};
         parseProgram();
-       
+
         // rules = {
         //     "s1": {
         //         "0": ["s1", "0", "R"],
@@ -342,9 +408,9 @@ $(document).ready(function () {
         //         "B": ["sh", "B", "R"]
         //     }
         // };
-       
-        let tape = new Tape("110110 101011");
-        
+
+        let tape = new Tape("1001001");
+
 
         let m = new Machine(tape, head, rules);
         m.runStatus = true;
